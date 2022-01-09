1a76137cc9d8e0f1f556f69a2783a4e9b7c8f314
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-01-09 10:32:51 -0300

    initial questions page

---
+++ lib/questions/question_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatelessWidget {
+  const QuestionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Trivia Game'),
+    );
+  }
+}
