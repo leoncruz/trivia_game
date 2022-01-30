2ca720dcaf9ccf0780f87a9de04563b0e756c2bd
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-01-30 14:27:17 -0300

    add a controller class for questions

---
+++ lib/contexts/questions/question_controller.dart
@@ -0,0 +1,40 @@
+import 'package:trivia_game/contexts/questions/question_model.dart';
+import 'package:trivia_game/contexts/questions/question_repository.dart';
+import 'package:trivia_game/contexts/questions/stores/alternative_store.dart';
+import 'package:trivia_game/contexts/questions/stores/question_store.dart';
+
+class QuestionController {
+  final QuestionStore _store = QuestionStore.instance;
+  final AlternativeStore _alternativeStore = AlternativeStore.instance;
+  final QuestionRepository _repository = QuestionRepository();
+  final int quantity = 5;
+  int index = 0;
+
+  static QuestionController instance = QuestionController();
+
+  QuestionModel get actualQuestion => _store.actualQuestion;
+
+  _changeQuestion() {
+    index++;
+
+    if (index < quantity) {
+      _store.changeQuestion(index);
+    } else {
+      _store.endQuestions();
+    }
+  }
+
+  compareAlternatives() {
+    // _alternativeStore.alternative == actualQuestion.correctAnswer;
+    _changeQuestion();
+  }
+
+  Future<void> fetchQuestions({required categoryId}) async {
+    final questions = await _repository.fetchQuestions(
+      categoryId: categoryId,
+      quantity: quantity,
+    );
+
+    _store.addQuestions(questions);
+  }
+}
