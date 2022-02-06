04857d0113f54e4ba56638ef1b32c5a0427ac94c
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-02-06 13:43:46 -0300

    end game screen

--- lib/contexts/questions/screens/end_game.dart
+++ lib/contexts/questions/screens/end_game.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:trivia_game/contexts/questions/stores/question_store.dart';
+import 'package:trivia_game/contexts/questions/question_controller.dart';
 
 class EndGame extends StatelessWidget {
   EndGame({Key? key}) : super(key: key);
 
-  final QuestionStore _questionStore = QuestionStore.instance;
+  final controller = QuestionController.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,15 +23,25 @@ class EndGame extends StatelessWidget {
                 fontSize: 26,
               ),
             ),
+            const SizedBox(height: 50),
             Text(
-              'Pontos: ${_questionStore.points}',
+              'Pontos: ${controller.points}',
               style: const TextStyle(
                 fontSize: 24,
               ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Início'),
+            const SizedBox(height: 50),
+            SizedBox(
+              width: 200,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/', (Route<dynamic> route) => false);
+                },
+                child: const Text('Voltar para o Início',
+                    style: TextStyle(fontSize: 18)),
+              ),
             ),
           ],
         ),
